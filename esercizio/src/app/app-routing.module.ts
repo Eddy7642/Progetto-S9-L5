ddecec441c161dacd7e6b64623e01ef1d9386698
Luca Chisari <75541078+Eddy7642@users.noreply.github.com>
2024-05-24 12:54:10 +0200

    Commit2 card

--- esercizio/src/app/app-routing.module.ts
+++ esercizio/src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AudiComponent } from './pages/audi/audi.component';
+import { FordComponent } from './pages/ford/ford.component';
+import { FiatComponent } from './pages/fiat/fiat.component';
+import { Pages404Component } from './pages/pages404/pages404.component';
 
 const routes: Routes = [
+  {
+path: '',
+redirectTo: '/home',
+pathMatch:'full'
+},
+{
+  path:"home",
+  component: HomeComponent,
+  title: "home"
+},
+{
+  path:"audi",
+  component: AudiComponent,
+  title: "audi"
+},
+{
+  path:"ford",
+  component: FordComponent,
+  title: "ford"
+},
+{
+  path:"fiat",
+  component: FiatComponent,
+  title: "fiat"
+},
+{
+  path:'**',
+  component:Pages404Component,
+  title:'error404'
+
+}
+
 
 
 ];
--- esercizio/src/app/app.module.ts
+++ esercizio/src/app/app.module.ts
@@ -5,6 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './main-component/navbar/navbar.component';
 import { FooterComponent } from './main-component/footer/footer.component';
+import { AudiComponent } from './pages/audi/audi.component';
+import { FiatComponent } from './pages/fiat/fiat.component';
+import { FordComponent } from './pages/ford/ford.component';
+import { HomeComponent } from './pages/home/home.component';
+import { Pages404Component } from './pages/pages404/pages404.component';
+
 
 
 @NgModule({
@@ -12,6 +18,11 @@ import { FooterComponent } from './main-component/footer/footer.component';
     AppComponent,
     NavbarComponent,
     FooterComponent,
+    AudiComponent,
+    FiatComponent,
+    FordComponent,
+    HomeComponent,
+    Pages404Component,
   ],
   imports: [
     BrowserModule,
